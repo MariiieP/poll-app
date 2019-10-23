ebdb7fbc82938af92e636c8931c408ff362ff9ba
MariiieP <33463825+MariiieP@users.noreply.github.com>
2019-10-23 17:24:31 +0300

    update SecurityConfig

--- src/main/java/com/poll/config/SecurityConfig.java
+++ src/main/java/com/poll/config/SecurityConfig.java
@@ -27,31 +27,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser("user").password("{noop}password").roles("USER");
     }
 
-
-    /*
-    *
-    *
-    @Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors
-						.basePackage("net.guides.springboot2.springboot2swagger2.controller"))
-				.paths(PathSelectors.regex("/.*"))
-				.build().apiInfo(apiEndPointsInfo());
-	}
-
-	private ApiInfo apiEndPointsInfo() {
-
-		return new ApiInfoBuilder().title("Spring Boot REST API")
-				.description("Employee Management REST API")
-				.contact(new Contact("Ramesh Fadatare", "www.javaguides.net", "ramesh24fadatare@gmail.com"))
-				.license("Apache 2.0")
-				.licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
-				.version("1.0.0")
-				.build();
-	}
-    *
-    * */
 }
 
 
